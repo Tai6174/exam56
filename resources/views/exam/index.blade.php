3c3884c3c3fa581eab56a71f34d04ff190b4e5fc
Tai6174 <40749579+Tai6174@users.noreply.github.com>
2018-07-05 13:23:42 +0800

    14 更新權限與標註已關閉測驗

--- resources/views/exam/index.blade.php
+++ resources/views/exam/index.blade.php
@@ -1,17 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('content')
-
-    <h1>測驗一覽 <small>（共 {{$exams->total()}} 筆資料）</small></h1>
+    <h1>所有測驗 <small>（共 {{ $exams->total() }} 筆資料）</small></h1>
     <ul class="list-group">
         @forelse($exams as $exam)
             <li class="list-group-item">
-                {{ $exam->created_at->format("Y年m月d日") }} -
-                <a href="exam/{{ $exam->id }}">
-                    {{ $exam->title }}
-                </a>
-                {{--  <a href="user/{{ Auth::id() }}">
-                    {{ Auth::name() }}
-                </a>  --}}
+                {{ $exam->created_at->format("Y年m月d日") }} - 
+                @if($exam->enable!=1)
+                    {{ bs()->badge()->text('關閉') }}
+                @endif
+                <a href="exam/{{ $exam->id }}">{{ $exam->title }}</a>
             </li>
         @empty
             <li class="list-group-item">尚無任何測驗</li>
@@ -20,4 +17,4 @@
     <div class="my-3">
         {{ $exams->links() }}
     </div>
-@endsection
+@stop
